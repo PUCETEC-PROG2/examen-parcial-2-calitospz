71884fd22b340e6a9915818831c6b572a64bd337
Carlos Pazmino Z <calitospz@gmail.com>
2025-01-11 10:46:13 -0500

    Update models.py

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.db import models
 
+
+class Movie(models.Model):
+    title = models.CharField(max_length=30, null=False)
+    genre = models.CharField(max_length=30, null=False)
+    director_name = models.CharField(max_length=30, null=False)
+    publication_year = models.CharField(max_length=30, null=False)
+    synopsis = models.TextField(null=False)
+    
+    def __str__(self):
+        return self.title
 # Create your models here.
