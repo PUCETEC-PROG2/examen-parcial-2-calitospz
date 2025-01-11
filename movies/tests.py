a6decba05ee826b31ce98f4756508b138c0b21b6
Carlos Pazmino Z <calitospz@gmail.com>
2025-01-11 10:46:54 -0500

    Update tests.py

--- movies/tests.py
+++ movies/tests.py
@@ -1,3 +1,35 @@
 from django.test import TestCase
+from django.test import TestCase
+from .models import Movie
+
+class MovieModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        # Create a movie object for testing
+        cls.movie = Movie.objects.create(
+            title='Inception',
+            genre='Sci-Fi',
+            director_name='Christopher Nolan',
+            publication_year=2010,
+            synopsis='A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O.'
+        )
+
+    def test_movie_title(self):
+        self.assertEqual(self.movie.title, 'Inception')
+
+    def test_movie_genre(self):
+        self.assertEqual(self.movie.genre, 'Sci-Fi')
+
+    def test_movie_director_name(self):
+        self.assertEqual(self.movie.director_name, 'Christopher Nolan')
+
+    def test_movie_publication_year(self):
+        self.assertEqual(self.movie.publication_year, 2010)
+
+    def test_movie_synopsis(self):
+        self.assertEqual(self.movie.synopsis, 'A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O.')
+
+# Create your tests here.
 
 # Create your tests here.
